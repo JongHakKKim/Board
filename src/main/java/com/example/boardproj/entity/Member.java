72425a950478b9479644de012dbf0f6dd9de9e55
aaaa <aaaa@a.a>
2024-11-14 17:20:00 +0900

    Member를 추가한 수정 커밋

---
+++ src/main/java/com/example/boardproj/entity/Member.java
@@ -0,0 +1,27 @@
+package com.example.boardproj.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mno;
+
+    @Column(length = 50, nullable = false)
+    private String title;
+
+    @Column(length = 255, nullable = false)
+    private String content;
+
+    @Column(length = 50, nullable = false)
+    private String writer;
+}
